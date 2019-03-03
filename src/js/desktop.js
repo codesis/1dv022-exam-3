fb029eda4c6cde5a7d270274fb6856c8e5f7a63b
emma <hassling.emma@gmail.com>
2019-03-03 22:00:14 +0100

    Adding code to Launcher & beginning w/ Desktop

--- src/js/desktop.js
+++ src/js/desktop.js
@@ -1,1 +1,31 @@
 'use strict'
+
+const Launcher = require('./launcher')
+
+function Desktop () {
+  this.activeWindow = false
+  this.mouseMovable = this.mouseMove.bind(this)
+  this.mouseUpFunc = this.mouseUp.bind(this)
+  this.windows = []
+  this.launcher = new Launcher(this)
+}
+// Initialize the desktop
+Desktop.prototype.init = function () {
+  this.launcher.init()
+
+  document.addEventListener('mousedown', this.mouseDown.bind(this))
+  document.addEventListener('keydown', this.keyDown.bind(this))
+}
+// When the mouse is released
+Desktop.prototype.mouseUp = function () {
+
+}
+// When the mouse is pressed
+Desktop.prototype.mouseDown = function (event) {
+
+}
+// When the mouse is moved
+Desktop.prototype.mouseMove = function (event) {
+
+}
+// When an icon/btn is clicked
--- src/js/launcher.js
+++ src/js/launcher.js
@@ -8,3 +8,97 @@ function Launcher (desktop) {
 Launcher.prototype.init = function () {
   document.querySelector('.launcher').addEventListener('click', this.launcherClick.bind(this))
 }
+Launcher.prototype.getClickedLauncherElement = function (target) {
+  let element
+
+  if (target.getAttribute('value')) {
+    element = target
+  } else if (target.parentNode.getAttribute('value')) {
+    element = target.parentNode
+  }
+
+  return element
+}
+
+Launcher.prototype.startApp = function (type, icon, title) {
+  let marginX = 10 * (this.desktop.offsetX)
+  let marginY = 10 * (this.desktop.offsetY)
+
+  let options = {
+    id: 'win-' + this.desktop.serialNumber,
+    x: marginX,
+    y: marginY,
+    tabIndex: this.desktop.serialNumber,
+    zIndex: this.desktop.zIndex,
+    icon: icon,
+    title: title,
+    maximizable: false,
+    keyActivated: false
+  }
+
+  let newApp = this.createApplication(type, options)
+
+  if (newApp) {
+    let buttons = document.querySelector('#' + newApp.id + ' .window-buttons')
+    buttons.addEventListener('click', this.desktop.clickWindowBtn.bind(this.desktop))
+
+    this.desktop.windows.push(newApp)
+
+    this.addRunningApp(type, newApp)
+
+    this.desktop.serialNumber += 1
+    this.desktop.offsetX += 1
+    this.desktop.offsetY += 1
+
+    this.desktop.setFocus(newApp.element)
+    this.checkBounds(newApp)
+  }
+}
+
+Launcher.prototype.createApplication = function (type, options) {
+  let newApp
+
+  switch (type) {
+    case 'memory': {
+      options.keyActivated = true
+      newApp = new Memory(options)
+      newApp.init()
+      break
+    }
+    case 'reset': {
+      this.desktop.clearDesktop()
+      break
+    }
+  }
+
+  return newApp
+}
+
+Launcher.prototype.checkBounds = function (app) {
+  let winWidth = window.innerWidth
+  let winHeight = window.innerHeight
+
+  let appRight = app.x + parseInt(app.element.offsetWidth)
+  let appBottom = app.y + parseInt(app.element.offsetHeight)
+
+  if (appRight > winWidth || app.x < 0) {
+    this.desktop.offsetX = 1
+    app.x = 10 * (this.desktop.offsetX)
+    app.element.style.left = app.x + 'px'
+  } else if (appBottom > winHeight || app.y < 0) {
+    this.desktop.offsetY = 1
+    app.y = 10 * (this.desktop.offsetY)
+    app.element.style.top = app.y + 'px'
+  }
+}
+
+Launcher.prototype.switchToWindow = function (id) {
+  let window = document.querySelector('#' + id)
+
+  if (window) {
+    if (window.classList.contains('minimized')) {
+      window.classList.remove('minimized')
+    }
+    this.desktop.setFocus(window)
+  }
+}
